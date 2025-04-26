d32d7df435e2382fce8c3abb867b656950f5bdf1
src_resources <src.res.211@gmail.com>
2025-04-26 19:47:13 +0800

    Chapter 33

---
+++ liblcthw/src/lcthw/list_algos.h
@@ -0,0 +1,12 @@
+#ifndef LCTHW_LIST_ALGOS_H
+#define LCTHW_LIST_ALGOS_H
+
+#include <lcthw/list.h>
+
+typedef int (*List_compare)(const void *a, const void *b);
+
+int List_bubble_sort(List *list, List_compare cmp);
+
+List *List_merge_sort(List *list, List_compare cmp);
+
+#endif /* LCTHW_LIST_ALGOS_H */
